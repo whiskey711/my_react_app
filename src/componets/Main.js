6336d3457e26b6152d36d74a50211e2063d78669
临风 许 <WarMachine2012@outlook.com>
2023-01-06 00:25:12 -0800

    getting started

---
+++ src/componets/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Main extends React.Component{
+    render(){
+        return (
+            <div id="mainpage">
+                <h1>Fun facts about React</h1>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was orignally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Main;
